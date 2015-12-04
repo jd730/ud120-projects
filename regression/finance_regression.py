413a8dffad342c932f02f41d70cd4be2442f0087
JaeD <jdhwang730@gmail.com>
2015-12-04 17:47:19 +0900

    Mini-project06, Enron data set bonus-salary Regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 
 """
-    Starter code for the regression mini-project.
+    starter code for the regression mini-project
     
-    Loads up/formats a modified version of the dataset
+    loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
-    that you'll find yourself in the outliers mini-project).
+    that you'll find yourself in the outliers mini-project)
 
-    Draws a little scatterplot of the training/testing data
+    draws a little scatterplot of the training/testing data
 
-    You fill in the regression code where indicated:
+    you fill in the regression code where indicated
+
 """    
 
 
@@ -21,7 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "salary"] ## salary -> long_term_incentive by JaeD
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,17 +30,20 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
-### plots it correctly. Don't forget to change the test_color above from "b" to
-### "r" to differentiate training points from test points.
-
-
-
+### plots it correctly. Don't forget to change the test_color from "b" to "r"
+### to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train,target_train)
+print reg.coef_
+print reg.intercept_
+print reg.score(feature_test,target_test)
 
 
 
@@ -52,6 +56,7 @@ for feature, target in zip(feature_test, target_test):
 for feature, target in zip(feature_train, target_train):
     plt.scatter( feature, target, color=train_color ) 
 
+
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
@@ -61,9 +66,13 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 ### draw the regression line, once it's coded
 try:
-    plt.plot( feature_test, reg.predict(feature_test) )
+   	plt.plot( feature_test, reg.predict(feature_test) ) 
+
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+print reg.coef_
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
