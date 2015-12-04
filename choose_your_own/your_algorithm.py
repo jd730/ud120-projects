860b5e385122513c239008528b0dd63b2df0dca8
JaeD <jdhwang730@gmail.com>
2015-12-04 17:38:12 +0900

    Mini-project04, Accuracy 0.94 at KNN(p=4, n=10, leaf size 50, ball tree, weight = distance)

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -7,9 +7,9 @@ from class_vis import prettyPicture
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
-### the training data (features_train, labels_train) have both "fast" and "slow"
-### points mixed together--separate them so we can give them different colors
-### in the scatterplot and identify them visually
+### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
+### in together--separate them so we can give them different colors in the scatterplot,
+### and visually identify them
 grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
 bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
 grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
@@ -25,7 +25,7 @@ plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
-################################################################################
+#################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
@@ -33,10 +33,18 @@ plt.show()
 
 
 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+#clf = KNeighborsClassifier(p=4,n_neighbors =10,leaf_size = 50,algorithm='ball_tree',weights='distance')
+#clf = RandomForestClassifier(n_estimators=100)
+clf = AdaBoostClassifier(n_estimators=100)
+clf.fit (features_train, labels_train)
 
+from sklearn.metrics import accuracy_score
 
-
-
+acc = accuracy_score(labels_test, clf.predict(features_test))
+print acc
 
 try:
     prettyPicture(clf, features_test, labels_test)
